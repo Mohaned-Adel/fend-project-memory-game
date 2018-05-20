fd8e8f798a967abfcff3caaa567eb3c16467ec1e
Mohaned-Adel <mohaned.adel1996@gmail.com>
2018-05-20 18:00:01 +0200

    fixing some bugs and edit README.md

--- README.md
+++ README.md
@@ -3,15 +3,24 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [Downlaod](#download)
+* [Rules](#rules)
 * [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+This is a Memory game project from udacity [Front-End](https://eg.udacity.com/course/front-end-web-developer-nanodegree--nd001) Nanodegree program, this game is very simple and fun, you can read the [Rules](#rules) to know how to play it, I hope you enjoy it.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Download
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+You can easily downlaod it and play it by just click on the download button and choose download .zip file, then you have to extract it, then open the index.html file and start playing.
+
+## Rules
+
+The rules are simple all you need is to find all the matching icons in less moves and times, you can only compare two icons in a move. If you get correct with two icons they will be open, and if you won't they will turn over again.
+If you finished the game with less than or equal ten moves you will get three stars, and if you finished it with more than 10 and less than 20 moves you will get 2 stars and if it was more than 20 and less than 30 you will get only one star, and if they were more than 30 you won't get any stars.
+you have to finished it before one hour.
+Good Luck.
 
 ## Contributing
 
--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
+var picked_cards = [];
+var cardValue = [];
+var tries = document.querySelector('.moves');
+var stars = document.querySelector('.stars');
+var restart = document.querySelector('.restart');
+var minuteLabel = document.getElementById('minutes');
+var secondLabel = document.getElementById('seconds');
+var cards = document.getElementsByClassName('card');
+var totalSeconds = 0;
+var sucess_move = 0;
+var starsCount = 3;
+var can_pick = true;
+
 /*
  * Create a list that holds all of your cards
  */
-var cards = document.getElementsByClassName('card');
-var cardValue = new Array();
 for (var i = 0; i < cards.length; i++){
     cardValue.push(cards[i].innerHTML);
 }
-var tries = document.querySelector('.moves');
 tries.innerHTML = 0;
 
 /*
@@ -24,28 +34,24 @@ function reset(){
     shuffle(cardValue);
     for (var i = 0; i < cards.length; i++) {
         cards[i].innerHTML = cardValue[i];
-        cards[i].classList.remove('match');
-        cards[i].classList.remove('show');
-        cards[i].classList.remove('open');
-        cards[i].classList.remove('shake');
-        cards[i].classList.remove('flipInY');
-        cards[i].classList.remove('rubberBand');
-        stars.childNodes[5].firstChild.classList.remove('far');
-        stars.childNodes[3].firstChild.classList.remove('far'); 
-        stars.childNodes[1].firstChild.classList.remove('far'); 
-        stars.childNodes[5].firstChild.classList.add('fa'); 
-        stars.childNodes[3].firstChild.classList.add('fa'); 
-        stars.childNodes[1].firstChild.classList.add('fa');          
+        cards[i].classList.remove('match', 'show', 'open', 'shake', 'flipInY', 'rubberBand');
+        for(var j=0; j < stars.childNodes.length; j++) { //Creating a separate for loop for the stars resetting.
+            if(stars.childNodes[j].firstChild) { // If the childNode has a firstChild property
+                if(stars.childNodes[j].firstChild.className) { // And if this firstChild has a className
+                    stars.childNodes[j].firstChild.classList.remove('far'); // remove far class from the list
+                    stars.childNodes[j].firstChild.classList.add('fa'); // add fa class to the list
+                }
+            }
+        }         
         cards[i].addEventListener('click',clicked);
     }
     tries.innerHTML = 0;
-    minuteLabel.innerHTML = 00;
-    secondLabel.innerHTML = 00;
+    minuteLabel.innerHTML = 0;
+    secondLabel.innerHTML = 0;
     totalSeconds = 0;
     sucess_move = 0;
 }
 
-var restart = document.querySelector('.restart');
 restart.addEventListener('click', reset);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -63,10 +69,7 @@ function shuffle(array) {
     return array;
 }
 
-// function for the timer
-var minuteLabel = document.getElementById('minutes');
-var secondLabel = document.getElementById('seconds');
-var totalSeconds = 0;
+// function for the timer if the first card was clicked
 setInterval(setTime, 1000);
 
 function setTime(){
@@ -94,11 +97,6 @@ function pad(val) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-var picked_cards = new Array();
-var can_pick = true;
-var sucess_move = 0;
-var starsCount = 3;
-var stars = document.querySelector('.stars');
 
 function clicked() {
     if(can_pick) {
@@ -113,12 +111,12 @@ function clicked() {
 
         if(picked_cards.length == 2) {
             tries.innerHTML++;
-            if (tries.innerHTML > 10){
+            if (tries.innerHTML > 10 && tries.innerHTML <= 20){
                 stars.childNodes[5].firstChild.classList.add('far'); 
                 stars.childNodes[5].firstChild.classList.remove('fa');
                 starsCount = 2;               
             } 
-            if (tries.innerHTML > 20 && tries.innerHTML < 30) {
+            if (tries.innerHTML > 20 && tries.innerHTML <= 30) {
                 stars.childNodes[5].firstChild.classList.add('far'); 
                 stars.childNodes[5].firstChild.classList.remove('fa');  
                 stars.childNodes[3].firstChild.classList.add('far'); 
@@ -139,19 +137,11 @@ function clicked() {
                 setTimeout(function(){
                     picked_cards[0].removeEventListener('click', clicked, false);
                     picked_cards[1].removeEventListener('click', clicked, false);
-                    picked_cards[0].classList.remove('open');
-                    picked_cards[0].classList.remove('show');
-                    picked_cards[1].classList.remove('open');
-                    picked_cards[1].classList.remove('show');
-                    picked_cards[0].classList.remove('flipInY');
-                    picked_cards[1].classList.remove('flipInY');
-                    picked_cards[0].classList.remove('rubberBand');
-                    picked_cards[1].classList.remove('rubberBand');                        
-                    picked_cards[0].classList.add('match');
-                    picked_cards[1].classList.add('match');
-                    picked_cards[0].classList.add('shake');
-                    picked_cards[1].classList.add('shake');
-                    picked_cards = new Array();
+                    picked_cards[0].classList.remove('open', 'show', 'flipInY', 'rubberBand');
+                    picked_cards[1].classList.remove('open', 'show', 'flipInY', 'rubberBand');                       
+                    picked_cards[0].classList.add('match', 'shake');
+                    picked_cards[1].classList.add('match', 'shake');
+                    picked_cards = [];
                     can_pick = true;
                     sucess_move++;
                     if(sucess_move == (cards.length/2)){
@@ -162,13 +152,9 @@ function clicked() {
                 setTimeout(function(){
                     picked_cards[0].classList.add('rubberBand');
                     picked_cards[1].classList.add('rubberBand');
-                    picked_cards[0].classList.remove('open');
-                    picked_cards[0].classList.remove('show');
-                    picked_cards[1].classList.remove('open');
-                    picked_cards[1].classList.remove('show');
-                    picked_cards[0].classList.remove('flipInY');
-                    picked_cards[1].classList.remove('flipInY');                    
-                    picked_cards = new Array();
+                    picked_cards[0].classList.remove('open', 'show', 'flipInY');
+                    picked_cards[1].classList.remove('open', 'show', 'flipInY');                  
+                    picked_cards = [];
                     can_pick = true;
                 }, 1000);
             }
@@ -179,16 +165,14 @@ function clicked() {
 function cong(){
     const mainDiv = document.getElementById('main');
     const finishDiv = document.getElementById('finish');
-    const playButton = document.querySelector('.play-again');
     const score = document.querySelector('.final-score');
+    const playButton = document.querySelector('.play-again');
     mainDiv.style.display = 'none';
     finishDiv.style.display = 'block';
     if(starsCount !== 1) {
-    score.innerHTML = "with " + tries.innerHTML + " Moves and " + starsCount 
-            + " stars and your final time is: " + minuteLabel.innerHTML + " minutes and " + secondLabel.innerHTML + " seconds";
+    score.innerHTML = "with " + tries.innerHTML + " Moves and " + starsCount + " stars and your final time is: " + minuteLabel.innerHTML + " minutes and " + secondLabel.innerHTML + " seconds";
     } else {
-    score.innerHTML = "with " + tries.innerHTML + " Moves and " + starsCount 
-            + " stars and your final time is: " + minuteLabel.innerHTML + " minutes and " + secondLabel.innerHTML + " seconds";        
+    score.innerHTML = "with " + tries.innerHTML + " Moves and " + starsCount + " stars and your final time is: " + minuteLabel.innerHTML + " minutes and " + secondLabel.innerHTML + " seconds";        
     }
     playButton.addEventListener('click', function(){
         finishDiv.style.display = 'none';
